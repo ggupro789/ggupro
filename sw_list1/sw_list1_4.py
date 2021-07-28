45998a2a60278145e6483d07d2ba618b3fcca88f
kyuchaellee <lbg2356@naver.com>
2021-07-28 20:16:29 +0900

    SW_list_1-9차시

--- sw_list1/sw_list1_4.py
+++ sw_list1/sw_list1_4.py
@@ -4,8 +4,21 @@ test_case = int(input())
 
 for i in range(test_case):
     x = list(map(int,input().split(' ')))
-    number_list = list(map(int,input().split(' ')))
+    number_list = map(int,input().split(' '))
+    maxmin = []
+ 
+    for k in range(len(number_list)-x[1]+1): # 전체 배열 - 묶는배열 크기 해서 indexerror 방지
+        sum = 0
+        for j in range(k,k+x[1]): # 묶는 배열 크기만큼 더하기
+
+            sum += number_list[j]
+            
+        maxmin.append(sum)
+    print("#{0} {1}".format(i+1,max(maxmin)-min(maxmin)))
+
+        
+
+
     
-    j = 0
-    for i in range(number_list):
-        sum =
+
+         
