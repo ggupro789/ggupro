72c0d3bf8875d074db23c8134ee3ab9a4f0ff671
kyuchaellee <lbg2356@naver.com>
2021-07-28 17:22:24 +0900

    SW_list_1-8번

---
+++ sw_list1/sw_list1_4.py
@@ -0,0 +1,11 @@
+#sw_list1_9차시 문제
+
+test_case = int(input())
+
+for i in range(test_case):
+    x = list(map(int,input().split(' ')))
+    number_list = list(map(int,input().split(' ')))
+    
+    j = 0
+    for i in range(number_list):
+        sum =
