67c6afd6386dd8c605df34d2b3e65d6fe299692f
kyuchaellee <lbg2356@naver.com>
2021-08-07 23:39:31 +0900

    2021-08-07

--- algorithm_file/DFS.py
+++ algorithm_file/DFS.py
@@ -1,7 +1,7 @@
 def dfs(start_v,visited_d : list): # DFS
     visited_d.append(start_v)
 
-    for node in sorted(graph[start_v]):
+    for node in graph[start_v]:
         if node not in visited_d:
             visited_d = dfs(node,visited_d)
     
---
+++ sw_stack/종이접기.py
@@ -0,0 +1,22 @@
+def sw_stack(N):
+    list = []
+    small_get = 0
+    while sum(list) < N/10 :
+        list.append(small_rec)
+        small_get += 1
+        
+        if(sum(list)<N/10):
+            
+
+
+
+
+test_case = int(input())
+
+for _ in range(test_case):
+    N = list(map(int(input())))
+
+small_rec = 10
+middle_rec = 20
+
+middle_get = 0
