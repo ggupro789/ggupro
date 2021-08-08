01d3aeb1485aa3f43cde6b3ad25975a1c9aeff24
kyuchaellee <lbg2356@naver.com>
2021-08-08 23:08:24 +0900

    2021-08-08

--- sw_stack/종이접기.py
+++ sw_stack/종이접기.py
@@ -1,14 +1,24 @@
 def sw_stack(N):
     list = []
-    small_get = 0
-    while sum(list) < N/10 :
-        list.append(small_rec)
-        small_get += 1
-        
-        if(sum(list)<N/10):
-            
+    count = 0
+    while sum(list) < N:
+        list.append(10)
 
+    small_get = len(list)
+    middle_get = 0
 
+    # while True :
+    #     list.pop()
+    #     small_get -= 1
+    #     if(len(list)==0):
+    #         break
+
+    #     list.append(20)
+
+    #     if(sum(list)>N):
+    #         list.pop()
+    #     elif(sum(list) == N):
+    #         count += (small_get+middle_get)*(middle_get
 
 
 test_case = int(input())
