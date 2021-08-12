db391e3568ea0f0b4ab5af7733a328dc512fadc1
kyuchaellee <lbg2356@naver.com>
2021-08-12 14:57:54 +0900

    2021-08-12

--- algorithm_baekjun/컨베이어 벨트 위 로봇 (20055번).py
+++ algorithm_baekjun/컨베이어 벨트 위 로봇 (20055번).py
@@ -14,21 +14,28 @@ for A in durability:
 
 count = 0
 howmany_count = 0
+
 while True:
-    visited_robot[0] = 1
+
+    visited_robot.appendleft(visited_robot.pop())  # 1 단계
+    deq.appendleft(deq.pop())
     visited_robot[-1] = 0
 
     for belt in range(N-2,-1,-1):
-        if visited_robot[belt] == 1 and visited_robot[belt+1] == 0 and deq[belt+1] != 0 :
+        if visited_robot[belt] == 1 and visited_robot[belt+1] == 0 and deq[belt+1] >= 1 :  # 2 단계
             deq[belt+1] -= 1
             visited_robot[belt+1] = 1
             visited_robot[belt] = 0
+    visited_robot[N-1] = 0
 
-    visited_robot.appendleft(visited_robot.pop())
-    deq.appendleft(deq.pop())
-    count = deq.count(0)
+    if deq[0] >= 1 and visited_robot[0] == 0:  # 3 단계
+        visited_robot[0] = 1
+        deq[0] -= 1
+
+
+    count = deq.count(0)  # 4 단계
     howmany_count += 1
     if count  >= K:
         break
-
+    
 print(howmany_count)
