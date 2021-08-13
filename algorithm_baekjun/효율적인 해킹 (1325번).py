09dd433aa32190f430204729c9ef31fa7082103f
kyuchaellee <lbg2356@naver.com>
2021-08-13 17:42:28 +0900

    2021-08-13

--- algorithm_baekjun/효율적인 해킹 (1325번).py
+++ algorithm_baekjun/효율적인 해킹 (1325번).py
@@ -3,17 +3,43 @@ from collections import deque
 def bfs(B):
     deq = deque()
     deq.append(B)
+    visited = [0 for _ in range(N+1)]
+    visited[B] = 1
+
+    count = 0
 
     while len(deq) > 0:
         B1 = deq.popleft()
-
-        for row in range(M):
-            if map_list[row][1] == map_list[B1][0] and visited[row] == 0 :
-                deq.append()
-
         
+        for j in map_list[B1]:
+            if visited[j] == 0 :
+                deq.append(j)
+                visited[j] = 1
+                count += 1
+
+    return count
+
 
 N,M = map(int,input().split())
-map_list = [list(map(int,input().split())) for _ in range(M)]
-count = []
-visited = [0 for _ in range(M)] # 행렬의 [1] 부분만 체크
+map_list = [[] for _ in range(N+1)]
+maxi = 0
+now = 0
+result = []
+
+for _ in range(M):
+    a,b = map(int,input().split())
+    map_list[b].append(a)
+
+for computer in range(1,N+1):
+    now = bfs(computer)
+    if now > maxi:
+        maxi = now
+        result = [computer]
+    elif now == maxi:
+        result.append(computer)
+
+for best in range(len(result)):
+    if best != len(result):
+        print("{0}".format(result[best]),end = " ")
+    else:
+        print("{0}".format(result[best]))
