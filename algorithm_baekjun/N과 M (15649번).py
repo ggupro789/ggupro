7c898d67881d5fd4ca5e4ada24676e3284d5f1a8
kyuchaellee <lbg2356@naver.com>
2021-08-15 18:11:49 +0900

    2021-08-15

--- algorithm_baekjun/N과 M (15649번).py
+++ algorithm_baekjun/N과 M (15649번).py
@@ -1,3 +1,20 @@
-N,M = map(int,input().split())
+def my_permutation(list,r,target):
+    if len(target) == r:
+        for item in target:
+            print(item,end=' ')
+        print()
+        return
+    
+    for i in range(len(list)):
+        if visited[i] == False:
+            visited[i] = True
+            my_permutation(list,r,target+[list[i]])
+            visited[i]= False
 
-def is_posible()
+N,M = map(int,input().split())
+map = []
+for i in range(N):
+    map.append(i+1)
+visited = [False for _ in range(N)]
+
+my_permutation(map,M,[])
