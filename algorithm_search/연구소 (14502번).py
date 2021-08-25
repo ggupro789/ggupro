5a7a5a846c5ec594aab145f5f6b1825b3823ee47
kyuchaellee <lbg2356@naver.com>
2021-08-25 23:17:08 +0900

    2021-08-25

--- algorithm_search/연구소 (14502번).py
+++ algorithm_search/연구소 (14502번).py
@@ -1,22 +1,61 @@
-# for item in itertools.permutations(temp_list,4):
-#     print(item)
-def bfs():
-    print
+import collections
 
-# def my_combination(list, k, r, target):
-#     if len(target) == r:
-#         #탈출
-#         print(target)
-#         return
+def bfs(row,col):
+    global map_list
 
-#     if k >= len(list): #  k의 역활 이해
-#         return
+    deq = collections.deque()
+    deq.append((row,col))
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    visited[row][col] = True
 
-    
-#     my_combination(list,k+1,r,target)
-#     my_combination(list,k+1,r,target+[list[k]])
+    dr = [0,0,-1,1]
+    dc = [-1,1,0,0]
+
+    while len(deq)>0:
+        row,col = deq.popleft()
+
+        for i in range(4):
+            nr = row + dr[i]
+            nc = col + dc[i]
+
+            if 0 <= nr < N and 0 <= nc < M:
+                if map_list[nr][nc] == 0:
+                    if visited[nr][nc] == False:
+                        deq.append((nr,nc))
+                        visited[row][col] = True
+                       
+
+    return 
+
+def my_combination(start,target):
+    global count
+    global map_list
+
+    count0 = 0
+    if target == 3:
+        #탈출
+        for row in range(N):
+            for col in range(M):
+                if map_list[row][col] == 2 :
+                    bfs(row,col,map_list)
+                    
+# 보류
+
+        return
+        
+    for i in range(start,N*M):
+        row = i // M
+        col = i % M
+        if map_list[row][col] == 0:
+            map_list[row][col] = 1
+            my_combination(i,target + 1)
+            map_list[row][col] = 0
 
 N,M = map(int,input().split())
 map_list = [list(map(int,input().split())) for _ in range(N)]
-visited = [[False for _ in range(M)] for _ in range(N)]
+count = 0
+
+my_combination(0,0)
+
+print(count)
 
