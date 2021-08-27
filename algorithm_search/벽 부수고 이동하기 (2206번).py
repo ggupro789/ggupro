f0ff6827786cc24804e215f25c3de62c4a145cde
kyuchaellee <lbg2356@naver.com>
2021-08-27 20:53:57 +0900

    2021-08-27

--- algorithm_search/벽 부수고 이동하기 (2206번).py
+++ algorithm_search/벽 부수고 이동하기 (2206번).py
@@ -1,55 +1,60 @@
-import collections
-import copy
+# import collections
 
-def bfs(row,col):
-    global count_short
 
-    deq = collections.deque()
-    deq.append((row,col))
-    visited = [[False for _ in range(M)] for _ in range(N)]
-    visited[row][col] = True
+# def bfs(row,col):
+#     global count_short
+#     count_1 = 0
+#     deq = collections.deque()
+#     deq.append((row,col,count_1))
+#     visited = [[[0,0] for _ in range(M)] for _ in range(N)]    
+    
+#     dr = [0,0,-1,1]
+#     dc = [-1,1,0,0]
 
-    dr = [0,0,-1,1]
-    dc = [-1,1,0,0]
-    count_in = 0
+#     while len(deq)>0:
+#         row,col,count_1,visited = deq.popleft()
 
-    while len(deq)>0:
-        row,col = deq.popleft()
+#         if row == N-1 and col == M-1:
+#             count_in = 0
+#             ans = []
+#             while True:
+#                 ans = visited[row][col]
+#                 count_in += 1
+#                 if ans == [0,1] or ans == [1,0]:
+#                     return count_in + 2
+#                 row = ans[0]
+#                 col = ans[1]
+    
+#         for i in range(4):
+#             nr = row + dr[i]
+#             nc = col + dc[i]
+            
+#             if 0 <= nr < N and 0 <= nc < M:
+#                 if map_list[nr][nc] == 1:
+#                     count_1 += 1
+#                     if count_1 == 1:
+#                         if visited[nr][nc] == [0,0] :
+#                             visited[nr][nc] = [row,col]
+#                             deq.append((nr,nc,count_1))
+                    
+#                     count_1 -= 1
 
-        if row == N-1 and col == M-1:
-            return count_in
-
-        for i in range(4):
-            nr = row + dr[i]
-            nc = col + dc[i]
-
-            if 0 <= nr < N and 0 <= nc < M:
-                if wall[nr][nc] == 0:
-                    if visited[nr][nc] == False:
-                        deq.append((nr,nc))
-                        visited[nr][nc] = True
-                        count_in += 1
-
-    return -1
+#                 elif map_list[nr][nc] == 0:
+#                     if visited[nr][nc] == [0,0]:
+#                         visited[nr][nc] = [row,col]
+#                         deq.append((nr,nc,count_1))
+                        
+#     return -1
 
          
+# N,M = map(int,input().split())
+# map_list = [list(map(int,input())) for _ in range(N)]
+# count = float('inf')
+# count_short = 0
 
-
-
-N,M = map(int,input().split())
-map_list = [list(map(int,input())) for _ in range(N)]
-wall = copy.deepcopy(map_list)
-count = float('inf')
-count_short = 0
-
-for row in range(N):
-    for col in range(M):
-        if wall[row][col] == 1:
-            wall[row][col] = 0
-            count_short = bfs(0,0)
-            wall[row][col] = 1
-
-            if count > count_short and count_short != -1:
-                count = count_short
-
-print(count)
+# count_short = bfs(0,0)
+# if count > count_short and count_short != -1:
+#     count = count_short
+# elif count_short == -1:
+#     count = count_short
+# print(count)
