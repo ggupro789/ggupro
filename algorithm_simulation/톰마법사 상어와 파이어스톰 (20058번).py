d90fe06d215829e9de01e702357fe59d7ffcb8d4
kyuchaellee <lbg2356@naver.com>
2021-09-04 18:21:17 +0900

    2021-09-04

--- algorithm_simulation/톰마법사 상어와 파이어스톰 (20058번).py
+++ algorithm_simulation/톰마법사 상어와 파이어스톰 (20058번).py
@@ -1,46 +1,31 @@
 import collections
 
-def bfs(row,col):
+def bfs2(row,col):
     deq = collections.deque()
     deq.append((row,col))
-    visited[row][col] = True
+    visited2[row][col] = True
+    ice_count = 1
 
     dr = [0,0,-1,1]
     dc = [-1,1,0,0]
 
-    ice_count = 1
-
     while len(deq) > 0:
-        
         row,col = deq.popleft()
-        near_ice = 0
 
         for i in range(4):
             nr = row + dr[i]
             nc = col + dc[i]
 
-            if 0 <= nr < map_count and 0 <= nc < map_count:
-                if visited[nr][nc] == False:
-                    if ice_map[nr][nc] != 0:
-                        visited[nr][nc] = True
-                        ice_count += 1
+            if 0 <= nr < map_count and  0 <= nc < map_count:
+                if visited2[nr][nc] == False:
+                    if map_list[nr][nc] != 0:
                         deq.append((nr,nc))
-
-                    elif ice_map[nr][nc] == 0:
-                        near_ice += 1
-                        
-                elif visited[nr][nc] == True:
-                    if ice_map[nr][nc] == 0:
-                        near_ice += 1
-                
-            elif nr < 0 or nr >= map_count or nc < 0 or nc >= nc:
-                near_ice += 1
-            
-        if near_ice >= 2:
-            ice_map[row][col] -= 1
-        
+                        visited2[nr][nc] = True
+                        ice_count += 1
+    
     return ice_count
 
+#---------------------------------------------- bfs
 N,Q = map(int,input().split())
 map_count = 1
 for _ in range(N):
@@ -48,43 +33,77 @@ for _ in range(N):
 map_list = [list(map(int,input().split())) for _ in range(map_count)]
 
 L = list(map(int,input().split()))
+deq2 = collections.deque()
 
+for i in L:
+    deq2.append(i)
 
-while len(L) > 0:
-    x = L[0]
-    L.remove(x)
-    visited = [[False for _ in range(map_count)] for _ in range(map_count)]
+#----------------------------------------------
 
-    ice_dong = 0
+while len(deq2) > 0:
+    x = deq2.popleft()
+
     rectangle = 1
+    
+    if x != 0:
+        for _ in range(x):
+            rectangle *= 2
+    elif x == 0 :
+        rectangle == 1
 
-    for _ in range(x):
-        rectangle *= 2
-    print(map_list)
     total_ice = 0
     ice_map = [[0 for _ in range(map_count)] for _ in range(map_count)]
-   
+
+
     for row in range(0,map_count,rectangle):
         for col in range(0,map_count,rectangle):
             for r in range(row,rectangle+row):
                 for c in range(col,rectangle+col):
-                    ice_map[c-col+row][row+col+rectangle-r-1] = map_list[r][c]
-    print(ice_map)
-    map_list = ice_map
-
+                    ice_map[row+c-col][row+col+rectangle-r-1] = map_list[r][c]
+    
+    dr = [0,0,-1,1]
+    dc = [-1,1,0,0]
+    all_cut = collections.deque()
     for row in range(map_count):
         for col in range(map_count):
             if ice_map[row][col] != 0:
-                if visited[row][col] == False:
-                    ice_k = bfs(row,col)
+                near_ice = 0
+                for i in range(4):
+                    nr = row + dr[i]
+                    nc = col + dc[i]
 
-            if ice_dong < ice_k:
-                ice_dong = ice_k
+                    if 0 <= nr < map_count and 0<= nc < map_count:
+                        if ice_map[nr][nc] == 0:
+                            near_ice += 1
+                    else:
+                        near_ice += 1
+                
+                if near_ice > 1:
+                    all_cut.append((row,col))
     
-    for row in range(map_count):
+    while len(all_cut) > 0 :
+        row,col = all_cut.popleft()
+        ice_map[row][col] -= 1
+
+    map_list = [item[:] for item in ice_map]
+    
+
+
+ice_dong = 0
+visited2 = [[False for _ in range(map_count)] for _ in range(map_count)]
+
+for row in range(map_count):
+    for col in range(map_count):
+        if map_list[row][col] != 0:
+            if visited2[row][col] == False:
+                ice_k = bfs2(row,col)
+                if ice_dong < ice_k:
+                    ice_dong = ice_k
+
+for row in range(map_count):
         for col in range(map_count):
-            total_ice += ice_map[row][col]
+            total_ice += map_list[row][col]
 
 
 print(total_ice)
-print(ice_k)
+print(ice_dong)
