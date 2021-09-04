f59ba3baf808606ab7a9ed922bb48b1a567d5a4c
kyuchaellee <lbg2356@naver.com>
2021-09-04 18:52:37 +0900

    2021-09-04

--- algorithm_search/아맞다우산 (17244번).py
+++ algorithm_search/아맞다우산 (17244번).py
@@ -1,5 +1,20 @@
+import collections
 
-while len(L) > 0:
-    x = L[0]
-    L.remove(x)
-    visited = [[False for
+def bfs(row,col):
+    deq = collections.deque()
+    deq.append(row,col)
+
+    dr = [0,0,-1,1]
+    dc = [-1,1,0,0]
+
+    
+
+N,M = map(int,input().split())
+
+map_list = [list(input()) for _ in range(M)]
+visited = [[False for _ in range(N)] for _ in range(M)]
+
+for row in range(N):
+    for col in range(M):
+        if map_list[row][col] == 'S':
+            bfs(row,col)
