6db00ce81da46232f403b78bdb94e39658306d96
kyuchaellee <lbg2356@naver.com>
2021-09-16 21:49:58 +0900

    2021-09-16

--- algorithm_file/컵라면 (1781번).py
+++ algorithm_file/컵라면 (1781번).py
@@ -1,34 +1,19 @@
+#https://velog.io/@ju_h2/Python-%EB%B0%B1%EC%A4%80-1781.-%EC%BB%B5%EB%9D%BC%EB%A9%B4-%ED%92%80%EC%9D%B4-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%83%90%EC%9A%95-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EA%B7%B8%EB%A6%AC%EB%94%94-%EA%B5%AC
 import heapq
-
-# test_list = [4, 3, 2, 5, 1]
-# print(test_list)
-
-# heapq.heapify(test_list)
-# print(test_list)
-# # heapq.heappush(test_list,3.5)
-
-# for i in range(5):
-#     a = heapq.heappop(test_list)
-#     print(a)
 
 N = int(input())
 map_list = []
-noodle_list = []
-index = 1
-result = 0
 
-for i in range(N):
+for _ in range(N):
     deadline,noodle = map(int,input().split())
-    map_list.append((deadline,noodle,i))
-    noodle_list.append((noodle))
-print(map_list)
-visited = [False for _ in range(N)]
+    map_list.append((deadline,noodle))
 
-heapq.heapify(noodle_list)
+map_list.sort()
+noodle_list = []
 
+for i in map_list:
+    heapq.heappush(noodle_list,i[1])
+    if i[0] < len(noodle_list):
+        heapq.heappop(noodle_list)
 
-
-for i in range(N):
-    a = heapq.heappop(noodle_list)
-    print(a)
-
+print(sum(noodle_list))
