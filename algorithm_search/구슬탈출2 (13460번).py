a24579b0393247f6dae39af31e0a3c23b7da18cb
kyuchaellee <lbg2356@naver.com>
2021-09-23 21:14:47 +0900

    2021-09-23

--- algorithm_search/구슬탈출2 (13460번).py
+++ algorithm_search/구슬탈출2 (13460번).py
@@ -25,13 +25,22 @@ def bfs():
 
             if 0 < nrR < N and 0 < ncR < M:
                 if map_list[nrR][ncR] != '#':
-
+                    if visited[nrR][ncR] == False:
+                        while map_list[nrR][ncR] != '#':
+                            nrR = nrR + dr[i]
+                            ncR = ncR + dc[i]
+                            visited[nrR][ncB] = True
+                            if map_list[nrR][ncR] == 'O':
+                                break
+                        deqR.append((nrR-dr[i],ncR-dc[i]))
+                    
 
 
 # -------------------------------------
 N,M = map(int,input().split())
 map_list = [list(input()) for _ in range(N)]
 visited = [[False for _ in range(M)] for _ in range(N)]
+count = 0
 
 for row in range(N):
     for col in range(M):
